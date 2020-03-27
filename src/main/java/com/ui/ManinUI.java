cf517c889cb71331a55bf0a00e4ae99388cd8733
fengjianche <shbyku@gmail.com>
2020-03-27 11:47:42 +0800

    测试冲突

--- src/main/java/com/ui/ManinUI.java
+++ src/main/java/com/ui/ManinUI.java
@@ -13,9 +13,6 @@ public class ManinUI {
 
     public static void main(String[] args) throws IOException {
         System.out.println("hello briefDBMS");
-
-
-
         while(true){
             System.out.println("1 选择数据库 当前数据:master");
             Scanner input = new Scanner(System.in);
@@ -30,13 +27,7 @@ public class ManinUI {
             //跳转语法解析工具，定位语法类型
             AnalysisUtil au= new AnalysisUtil();
             au.grammarPositon(sql);
-            System.out.printf("yelin");
-            System.out.println("2020/3/27");
-
+            System.out.println("hello");
         }
-
-
-
-
     }
 }
