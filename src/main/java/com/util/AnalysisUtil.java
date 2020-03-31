ded28224b76db0cc5276c29a34582899c4ca6036
fengjianche <shbyku@gmail.com>
2020-03-31 11:04:05 +0800

    Version_3.30.2130        修复了解析工具->判断表存在的方法        实现日志写入功能，调用LogRecord类下的功能会自动封装sql语句写入日志文件        新增insert命令的无列名判断        尚未解决的问题：            暂时不支持对master数据库进行任何操作！            若用户在master数据库下建表的情况            建立重复库的判断            若是主键的情况下，应自动添加NOT NULL约束            关于用户名的问题：                权限控制框架搭建，但是并未完全实现。可以建立 D:\BRIEFDBMS\account\***.txt 文件，该文件作会作为一个新增的账户，密码为空。        项目可能存在的问题：            创建表时，列名和属性的完整性检验可能存在问题，表现为创建表时，CreateTable类在分离参数时发生未知错误            目录结构为封装，需要手动建立，下个版本会解决目录结构问题

--- src/main/java/com/util/AnalysisUtil.java
+++ src/main/java/com/util/AnalysisUtil.java
@@ -63,6 +63,7 @@ public class AnalysisUtil {
     }
 
     //判断数据库中是否含有一张表
+    //传入的参数 tablename为表名，filename为当前数据库的路径 直接调用全局变量即可传入
     public boolean isHaveTheTable(String tableName,String databaseName,String filePath)
     {
         //获取目录下所有文件名
